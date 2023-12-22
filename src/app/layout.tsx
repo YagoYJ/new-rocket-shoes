945272d2f6b22b67badbec4e8718aa500e130e1a
Yago <yagojuanyjskt@gmail.com>
2023-12-22 15:49:11 -0300

    feat: Create the Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='w-11/12 max-w-7xl flex flex-col items-center mx-auto'>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import logoImage from "@/assets/images/logo.svg"
+import Image from "next/image"
+import { Cart } from "./Cart"
+import Link from "next/link"
+
+export function Header () {
+    return (
+        <header className="w-full flex items-center justify-between py-8">
+            <Link href='/'>
+                <Image src={logoImage} alt="Rocket Shoes" />
+            </Link>
+
+            <Cart />
+        </header>
+    )
+}
