1fa82ad80fcd1b59ee67f665d014af1b6728c241
Yago <yagojuanyjskt@gmail.com>
2023-12-28 12:09:11 -0300

    fix: Translate products names

--- src/utils/products.tsx
+++ src/utils/products.tsx
@@ -8,37 +8,37 @@ export type Product = {
 export const products: Product[] = [
     {
       id: "dbee2693-4af0-4c2d-baf6-0efff473c60b",
-      title: "Tênis de Caminhada Leve Confortável",
+      title: "Light and Comfortable Walking Shoes",
       price: 179.9,
       image: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg"
     },
     {
       id: "d1dae854-d97c-47cd-813d-499caf4aa5e8",
-      title: "Tênis VR Caminhada Confortável Detalhes Couro Masculino",
+      title: "VR Comfortable Walking Shoes, Leather Details for Men",
       price: 139.9,
       image: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg"
     },
     {
       id: "fb14b40e-0efd-47bc-b6eb-ba43a634b2f9",
-      title: "Tênis Adidas Duramo Lite 2.0",
+      title: "Adidas Duramo Lite 2.0 Shoes",
       price: 219.9,
       image: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg"
     },
     {
       id: "dc2f2bf6-cad6-4634-9595-e716932446b6",
-      title: "Tênis de Caminhada Leve Confortável",
+      title: "Light and Comfortable Walking Shoes",
       price: 179.9,
       image: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg"
     },
     {
       id: "fcfaaccf-74a6-4291-b778-9b99b2775050",
-      title: "Tênis VR Caminhada Confortável Detalhes Couro Masculino",
+      title: "VR Comfortable Walking Shoes, Leather Details for Men",
       price: 139.9,
       image: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg"
     },
     {
       id: "7eb82790-d236-482a-9529-6c6f20313c53",
-      title: "Tênis Adidas Duramo Lite 2.0",
+      title: "Adidas Duramo Lite 2.0 Shoes",
       price: 219.9,
       image: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg"
     }
